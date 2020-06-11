3e2113d5612891ca1add736c701dc67f006d41f5
ricardo <alucard_invidia@hotmail.com>
2020-06-11 16:43:50 +0800

    update VUEDEMO

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
+Vue.component('grocery-item', {
+  props: ['grocery'],
+  template: '<li>{{ grocery.text  }}</li>'
 })
 
 var componentListDemo = new Vue({
@@ -37,6 +37,47 @@ var example2 = new Vue({
         alert(event.target.tagName);
         console.log(JSON.stringify(event));
       }
+    }
+  }
+})
+
+
+var watchExampleVM = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
+  },
+  watch: {
+    // 如果 `question` 发生改变，这个函数就会运行
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function () {
+    // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+    // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+    // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+    // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+    // 请参考：https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
     }
   }
 })
@@ -152,3 +193,160 @@ var listenChildComponent = new Vue({
     postFontSize: 1
   }
 })
+
+/**
+ * anonymous slot
+ **/
+// Vue.component('navigation-link', {
+//   template: `<navigation-child>
+//   </navigation-child>`
+// })
+
+// Vue.component('navigation-child', {
+//   template: `<template>
+//   <div class="frame">
+//   <slot>default</slot>
+//   <p>childtext</p>
+//   </div>
+//   </template>`
+// })
+
+
+// var slotComponent = new Vue({
+//   el: '#slot-demo',
+//   data: {}
+// })
+
+/**
+ * unanonymous slot
+ **/
+// Vue.component('homepage-parent', {
+//   template: `<homepage-child>
+//   <template  v-slot:header>My Image’s Title</template>
+//   <template  v-slot:default>My Image’s Title</template>
+//   </homepage-child>`
+// })
+
+// Vue.component('homepage-child', {
+//   template: `<div class="container">
+//   <header>
+//     <slot name="header"></slot>
+//   </header>
+//   <main>
+//     <slot></slot>
+//   </main>
+//   <footer>
+//     <slot name="footer"></slot>
+//   </footer>
+// </div>`
+// })
+
+// var slotComponent = new Vue({
+//   el: '#noble-slot-demo',
+//   data: {}
+// })
+
+Vue.component('scope-slot', {
+  template: `<div id='scope-slot-child-demo'>
+    <span>
+    <slot v-bind:user="user">
+    {{ user.lastName }}
+    </slot>
+    </span>
+    </div>`
+});
+
+Vue.component('scope-parent-slot', {
+  template: `<template>
+    <current-user>
+      <template v-slot:default="slotProps">{{ slotProps.user.firstName }}</template>    
+    </current-user>
+    </template>`
+});
+
+var vue = new Vue({
+  el: '#scope-slot-child-demo',
+  data: {
+    user: {
+      lastName: 'ricardo',
+      firstName: 'li'
+    }
+  }
+});
+
+var Vue = new Vue({
+  el: '#scope-slot-demo',
+  data: {
+    user: {
+      lastName: 'ricardo',
+      firstName: 'li'
+    }
+  }
+});
+
+// Vue.component('my-component', {
+//   props: {
+//     // 基础的类型检查 (`null` 和 `undefined` 会通过任何类型验证)
+//     propA: Number,
+//     // 多个可能的类型
+//     propB: [String, Number],
+//     // 必填的字符串
+//     propC: {
+//       type: String,
+//       required: true
+//     },
+//     // 带有默认值的数字
+//     propD: {
+//       type: Number,
+//       default: 100
+//     },
+//     // 带有默认值的对象
+//     propE: {
+//       type: Object,
+//       // 对象或数组默认值必须从一个工厂函数获取
+//       default: function () {
+//         return {
+//           message: 'hello'
+//         }
+//       }
+//     },
+//     // 自定义验证函数
+//     propF: {
+//       validator: function (value) {
+//         // 这个值必须匹配下列字符串中的一个
+//         return ['success', 'warning', 'danger'].indexOf(value) !== -1
+//       }
+//     }
+//   },
+//   template: `
+//   <div>
+//     <p>{{ propA }}</p>
+//     <p>{{ propB }}</p>
+//     <p>{{ propC }}</p>
+//     <p>{{ propD }}</p>
+//     <p>{{ propE }}</p>
+//     <p>{{ propF }}</p>
+//   </div>`
+// })
+
+// var validatorComponent = new Vue({
+//   el: '#validator-demo',
+//   data: {
+//     propA: null,
+//     propB: 'String',
+//     propC: 'String123142',
+//     propD: 1000,
+//     propE: {},
+//     propF: 'success'
+//     // 必填的字符串
+//     // propC: {
+//     //   type: 'String',
+//     //   required: true
+//     // },
+//     // 带有默认值的数字
+//     // propD: {
+//     //   type: 1,
+//     // },
+//     // 带有默认值的对象
+//   }
+// })
